96afd169fd571b819beac9e52d34aa7c2d8e0510
suyogdahal <suyogdahal46@gmail.com>
2022-01-15 15:45:35 +0545

    Renamed old files

---
+++ calculator.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import re
+choice=True
+previous=0
+def calculate():
+    global choice
+    global previous
+    if previous==0:
+        equation=input("Enter the equation: ")
+    else:
+        equation=input(str(previous))
+    if equation=='quit':
+        choice=False
+    else:
+        new_equation=re.sub('[a-zA-z,():" "]', '' , equation)
+        if previous==0:
+            previous=eval(new_equation)
+            print(previous)
+        else:
+            previous=eval(str(previous) + equation )
+while choice:
+    calculate()
+
