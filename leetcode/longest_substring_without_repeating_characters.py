2f144b90753a426c84947328ecf9e307198c1f25
suyogdahal <suyogdahal46@gmail.com>
2022-05-26 22:00:57 +0545

    Decrease time complexity of longest_substring problem to O(n)

--- leetcode/longest_substring_without_repeating_characters.py
+++ leetcode/longest_substring_without_repeating_characters.py
@@ -1,23 +1,23 @@
 # Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+from operator import sub
 from typing import List, Tuple
 
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> Tuple[set, int]:
-        substrings = set()
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substring = ""
         greatest_length = 1 if len(s) else 0
-        for idx1, char1 in enumerate(s):
-            idx2 = min(idx1 + 1, len(s))
-            for char2 in s[idx2:]:
-                if char2 in char1:
-                    break
-                char1 += char2
-                if len(char1) > greatest_length:
-                    substrings.add(char1)
-                    greatest_length = len(char1)
-        return substrings, greatest_length
+        for char in s:
+            if char in substring:
+                char_idx_in_substring = substring.index(char)
+                idx = min(char_idx_in_substring + 1, len(substring))
+                substring = substring[idx:]
+            substring += char
+            if len(substring) > greatest_length:
+                greatest_length = len(substring)
+
+        return greatest_length
 
 
-substrings, greatest_length = Solution().lengthOfLongestSubstring("anijajaj")
-print(substrings)
+greatest_length = Solution().lengthOfLongestSubstring("abcabcbb")
 print(greatest_length)
